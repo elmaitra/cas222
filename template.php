9fbf5e8dcbf6d9b4580cad2991a2fdce803217e6
elmaitra <38444649+elmaitra@users.noreply.github.com>
2018-05-10 02:22:00 -0700

    Add files via upload

--- template.php
+++ template.php
@@ -1,7 +1,7 @@
 <!doctype html>
 
 <!--Assignment Name: CAS 222 Ace Website Template
-File Name: template.html
+File Name: template.php
 Date: 05/03/18
 Programmer: Ellorashree Maitra
 -->
@@ -20,78 +20,63 @@ Programmer: Ellorashree Maitra
 <link rel="stylesheet" href="css/reset.css" type="text/css">
 <link rel="stylesheet" href="css/helper.css" type="text/css">
 <link rel="stylesheet" href="css/grid.css" type="text/css">
+<link rel="stylesheet" href="css/navigation.css" type="text/css">
 <link rel="stylesheet" href="css/style.css" type="text/css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> <!--This links to the external font-awesome stylesheet. I used several fa icons in this code for decoration. Cheatsheet here: http://fontawesome.io/cheatsheet/ To improve web accessibility, always use "aria-hidden="true" and provide a descriptive title. Also, you can make additional styling alterations to the fa class in the css file to make the icons consistent in style with the overall look of the page. -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-</script>
-    
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!--This links to the external font-awesome stylesheet. I used several fa icons in this code for decoration. Cheatsheet here: http://fontawesome.io/cheatsheet/ To improve web accessibility, always use "aria-hidden="true" and provide a descriptive title. Also, you can make additional styling alterations to the fa class in the css file to make the icons consistent in style with the overall look of the page. -->
+   
 </head>
 
 <body>
 
 <?php include 'includes/header.inc.html.php';?>
-
-<nav class="menu">
- <ul>
-    <li>Home</li>
-    <li>Events</li>
-    <li>Register</li>
-    <li>Contact</li>
-    <li>FAQs</li>
-  </ul>   
+    
+<nav class="topnav" id="myTopnav">
+    
+    <a href="#home" class="active">Home</a>
+    <a href="#events">Events</a>
+    <a href="#register">Register</a>
+    <a href="#contact">Contact</a>
+    <a href="#faqs">FAQs</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+    </a>
 </nav>
-   
-<script>
-    
-$( document ).ready(function() {
 
-$( ".cross" ).hide();
-$( ".menu" ).hide();
-$( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".hamburger" ).hide();
-$( ".cross" ).show();
-});
-});
-
-$( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".cross" ).hide();
-$( ".hamburger" ).show();
-});
-});
-
-});
-    
-</script>
     
 <main>
-  
-<h2>Lorem Ipsum</h2>
+ <div class="hero">
+     <h3>Hero image or slideshow goes here.</h3>
+     </div>    
     
-<section>    
-
-    
-<p>Some text here.
-</p>
-
-   
+<section class="section group">
+ <div class="col span_8_of_12">
+    <h2>Lorem Ipsum</h2>
+    <p>Some text here.</p>
+    <p>Some more text.</p>
+    </div>
 </section>
-    
-<section>
-    
- <h2>More Lorem Ipsum</h2> 
+    <section class="section group">
+    <div class="col span_8_of_12">
+        <h2>More Lorem Ipsum</h2>
     <ul>
-    <li>List Item</li>
-    <li>List Item</li>
-    <li>List Item</li>
+        <li>List Item.</li>
+        <li>List Item.</li>
     </ul>
-
-</section>
+        </div>
+    </section>
     
+    <section id="sidebar" class="section group">
+        <div class="colspan_4_of_12">
+        <p>Social Media <br>
+            and <br>
+          Weather feeds go here.</p>
+    </div>
+    </section>
 </main>
     
 <?php include 'includes/footer.inc.html.php';?>
+
+<script src="js/scripts.js"></script>
       
 </body>
 
