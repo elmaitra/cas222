c505c40b8070844ffaefc77a110fc9022ee8c248
elmaitra <38444649+elmaitra@users.noreply.github.com>
2018-05-10 02:21:10 -0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-var currentYear = new Date().getFullYear();
-document.getElementById("thisYear").innerHTML = currentYear;
-
+function myFunction() {
+       var x = document.getElementById("myTopnav");
+       if (x.className === "topnav") {
+           x.className += " responsive";
+       } else {
+           x.className = "topnav";
+       }
+   }
