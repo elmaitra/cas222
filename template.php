69473d052be3791aff2f26c47dc921fa7e3a245f
elmaitra <38444649+elmaitra@users.noreply.github.com>
2018-05-13 23:38:22 -0700

    Add files via upload

--- template.php
+++ template.php
@@ -23,58 +23,78 @@ Programmer: Ellorashree Maitra
 <link rel="stylesheet" href="css/navigation.css" type="text/css">
 <link rel="stylesheet" href="css/style.css" type="text/css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!--This links to the external font-awesome stylesheet. I used several fa icons in this code for decoration. Cheatsheet here: http://fontawesome.io/cheatsheet/ To improve web accessibility, always use "aria-hidden="true" and provide a descriptive title. Also, you can make additional styling alterations to the fa class in the css file to make the icons consistent in style with the overall look of the page. -->
+<script src="https://code.jquery.com/jquery-3.2.0.min.js"></script>
    
 </head>
 
 <body>
 
-<?php include 'includes/header.inc.html.php';?>
-    
-<nav class="topnav" id="myTopnav">
-    
-    <a href="#home" class="active">Home</a>
-    <a href="#events">Events</a>
-    <a href="#register">Register</a>
-    <a href="#contact">Contact</a>
-    <a href="#faqs">FAQs</a>
-    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-    </a>
-</nav>
-
+<nav>
+    <?php include 'includes/navigation.inc.html.php';?>
+        </nav>
+<header>
+    <?php include 'includes/header.inc.html.php';?>
+</header>
     
 <main>
- <div class="hero">
-     <h3>Hero image or slideshow goes here.</h3>
-     </div>    
+    <section class="primary">
+<h2>An quod ita callida est, ut optime possit architectari voluptates?</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Negat enim summo bono afferre incrementum diem. Nunc de hominis summo bono quaeritur; Duo Reges: constructio interrete. Sumenda potius quam expetenda. </p>
     
-<section class="section group">
- <div class="col span_8_of_12">
-    <h2>Lorem Ipsum</h2>
-    <p>Some text here.</p>
-    <p>Some more text.</p>
-    </div>
+<h3>Sit enim idem caecus, debilis.</h3>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tu quidem reddes; Pauca mutat vel plura sane; Traditur, inquit, ab Epicuro ratio neglegendi doloris. Hoc est non dividere, sed frangere. Quid est igitur, inquit, quod requiras? Duo Reges: constructio interrete.</p>
+
+<h3>Sit enim idem caecus, debilis</h3>
+    
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tu quidem reddes; Pauca mutat vel plura sane; Traditur, inquit, ab Epicuro ratio neglegendi doloris. Hoc est non dividere, sed frangere. Quid est igitur, inquit, quod requiras? Duo Reges: constructio interrete.</p>
+<ul>   
+<li>Hoc est non modo cor non habere, sed ne palatum quidem.</li>
+<li>Consequens enim est et post oritur, ut dixi.</li>
+<li>Quod dicit Epicurus etiam de voluptate, quae minime sint voluptates, eas obscurari saepe et obrui.</li>
+</ul>
+        
+<h3>Sit enim idem caecus, debilis</h3>
+    
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tu quidem reddes; Pauca mutat vel plura sane; Traditur, inquit, ab Epicuro ratio neglegendi doloris. Hoc est non dividere, sed frangere. Quid est igitur, inquit, quod requiras? Duo Reges: constructio interrete.</p>
+<ul>   
+<li>Hoc est non modo cor non habere, sed ne palatum quidem.</li>
+<li>Consequens enim est et post oritur, ut dixi.</li>
+<li>Quod dicit Epicurus etiam de voluptate, quae minime sint voluptates, eas obscurari saepe et obrui.</li>
+</ul>
+        
+<h3>Sit enim idem caecus, debilis</h3>
+    
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tu quidem reddes; Pauca mutat vel plura sane; Traditur, inquit, ab Epicuro ratio neglegendi doloris. Hoc est non dividere, sed frangere. Quid est igitur, inquit, quod requiras? Duo Reges: constructio interrete.</p>
+<ul>   
+<li>Hoc est non modo cor non habere, sed ne palatum quidem.</li>
+<li>Consequens enim est et post oritur, ut dixi.</li>
+<li>Quod dicit Epicurus etiam de voluptate, quae minime sint voluptates, eas obscurari saepe et obrui.</li>
+</ul>
+        
 </section>
-    <section class="section group">
-    <div class="col span_8_of_12">
-        <h2>More Lorem Ipsum</h2>
-    <ul>
-        <li>List Item.</li>
-        <li>List Item.</li>
-    </ul>
-        </div>
-    </section>
     
-    <section id="sidebar" class="section group">
-        <div class="colspan_4_of_12">
-        <p>Social Media <br>
-            and <br>
-          Weather feeds go here.</p>
-    </div>
-    </section>
+<section class="sidebar"> 
+    
+<h3>Sit enim idem caecus, debilis</h3>
+    
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tu quidem reddes; Pauca mutat vel plura sane; Traditur, inquit, ab Epicuro ratio neglegendi doloris. Hoc est non dividere, sed frangere. Quid est igitur, inquit, quod requiras? Duo Reges: constructio interrete.</p>
+<ul>   
+<li>Hoc est non modo cor non habere, sed ne palatum quidem.</li>
+<li>Consequens enim est et post oritur, ut dixi.</li>
+<li>Quod dicit Epicurus etiam de voluptate, quae minime sint voluptates, eas obscurari saepe et obrui.</li>
+</ul>
+
+</section>
+
+
+
 </main>
     
-<?php include 'includes/footer.inc.html.php';?>
+<footer>
+    <?php include 'includes/footer.inc.html.php';?>
+</footer>
 
 <script src="js/scripts.js"></script>
       
